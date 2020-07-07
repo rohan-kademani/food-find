44795d6948b5ccac5760b4866e158c771888ceed
rohan <rohankademani@outlook.in>
2020-07-07 23:07:17 +0530

    Updates

---
+++ precache-manifest.d841308bc69cd601542826c0291ddcfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b6798bff4bd9692891a0337156efcef",
+    "url": "/food-find/index.html"
+  },
+  {
+    "revision": "0642506124ba5690974d",
+    "url": "/food-find/static/css/main.00c7115d.chunk.css"
+  },
+  {
+    "revision": "25f98137d09db5ca3d88",
+    "url": "/food-find/static/js/2.466e7126.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/food-find/static/js/2.466e7126.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0642506124ba5690974d",
+    "url": "/food-find/static/js/main.498edf57.chunk.js"
+  },
+  {
+    "revision": "6c5766eda846943feedc",
+    "url": "/food-find/static/js/runtime-main.d96eace3.js"
+  }
+]);
